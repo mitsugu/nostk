986541897e556492fa0c649f78428ba903f0874a
mitsugu oyama <mitsugu.oyama@gmail.com>
2024-05-12 10:25:20 +0900

    add removeEvent

--- nostk.go
+++ nostk.go
@@ -125,6 +125,11 @@ func main() {
 			log.Fatal(err)
 			os.Exit(1)
 		}
+	case "removeEvent":
+		if err := removeEvent(os.Args); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
 	default:
 		log.Fatal(errors.New("Subcommand does not exist."))
 		os.Exit(1)
@@ -647,7 +652,93 @@ func catSelf(args []string) error {
 // }}}
 
 /*
-emojiRreaction
+removeEvent
+	[infomation for develop]
+	usage:
+		nostk removeEvent <event_id>
+	kind: 5
+	content: reason text (must)
+	tags [
+		"e": event id (hex)
+	]
+*/
+func removeEvent(args []string) error {
+	var event_id string
+	content :=""
+
+	if len(args)<3 || 4<len(args) {
+		return errors.New("Wrong number of parameters")
+	}
+	for i := range args {
+		if i<2 {
+			continue
+		}
+		switch i {
+		case 2: // event_id
+			event_id = args[i]
+		case 3: // content
+			content = args[i]
+		}
+	}
+
+	sk, err := load(hsec)
+	if err != nil {
+		fmt.Println("Nothing key pair. Make key pair.")
+		return err
+	}
+	pk, err := nostr.GetPublicKey(sk)
+	if err != nil {
+		return err
+	}
+
+	var rl []string
+	if err := getRelayList(&rl); err != nil {
+		fmt.Println("Nothing relay list. Make a relay list.")
+		return err
+	}
+
+	var t []string
+	tgs := nostr.Tags{}
+	if err := setCustomEmoji(content, &tgs); err != nil {
+		return err
+	}
+	t = nil
+	t = append(t, "e")
+	t = append(t, event_id)
+	tgs = append(tgs, t)
+
+	ev := nostr.Event{
+		PubKey:    pk,
+		CreatedAt: nostr.Now(),
+		Kind:      nostr.KindDeletion,
+		Tags:      tgs,
+		Content:   content,
+	}
+
+	// calling Sign sets the event ID field and the event Sig field
+	ev.Sign(sk)
+
+	// publish the event to two relays
+	ctx := context.Background()
+	for _, url := range rl {
+		relay, err := nostr.RelayConnect(ctx, url)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		_, err = relay.Publish(ctx, ev)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Printf("published to %s\n", url)
+	}
+
+	return nil
+}
+
+/*
+emojiRreaction {{{
 	[infomation for develop]
 	usage:
 		nostk emojiReaction <event_id> <public_key> <content>
@@ -748,6 +839,7 @@ func emojiReaction(args []string) error {
 
 	return nil
 }
+// }}}
 
 /*
 getDir {{{
